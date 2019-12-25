aaab12e8e9ab8dbc7c8410628e0f1479ca4797e9
vicco <realvicco@outlook.com>
2019-12-25 23:52:53 +0800

    init

---
+++ src/Solution.java
@@ -0,0 +1,18 @@
+public class Solution{
+    public static void main(String[] args) {
+        int a=1;
+        int b=a+1;
+        System.out.println(b);
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        aa();
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+
+    }
+    public static void aa(){
+        System.out.println("aa");
+    }
+
+}
